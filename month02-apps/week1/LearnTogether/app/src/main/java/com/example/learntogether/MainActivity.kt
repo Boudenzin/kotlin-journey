b18bf56c626c75d421601fbc2a26d41322b6a9a2
romildo <romildo-rrj@hotmail.com>
2025-06-04 21:00:22 -0300

    refac: Altere a posição dos imports

--- month02-apps/week1/LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
+++ month02-apps/week1/LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -13,16 +13,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.learntogether.ui.theme.LearnTogetherTheme
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 
 class MainActivity : ComponentActivity() {
