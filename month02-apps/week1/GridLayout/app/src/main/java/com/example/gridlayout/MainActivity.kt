54ec5734968a267da1246428f826e341073e3a3c
romildo.rodrigues <romildo-rrj@hotmail.com>
2025-06-12 14:17:50 -0300

    feat: Adiciona a lógica principal do exercício

--- month02-apps/week1/GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.kt
+++ month02-apps/week1/GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gridlayout.ui.theme.GridLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GridLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    GridLayout(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GridLayout(modifier: Modifier = Modifier) {
+    Column {
+        Row {
+            Column {
+                Text(
+                    text = stringResource(id = R.string.title_1),
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(id = R.string.title_1_subtitle)
+                )
+            }
+            Column {
+                Text(
+                    text = stringResource(id = R.string.title_2),
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(id = R.string.title_2_subtitle)
+                )
+            }
+        }
+        Row {
+            Column {
+                Text(
+                    text = stringResource(id = R.string.title_3),
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(id = R.string.title_3_subtitle)
+                )
+            }
+            Column {
+                Text(
+                    text = stringResource(id = R.string.title_3),
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(id = R.string.title_3_subtitle)
+                )
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GridLayoutTheme {
-        Greeting("Android")
+        GridLayout()
     }
 }
