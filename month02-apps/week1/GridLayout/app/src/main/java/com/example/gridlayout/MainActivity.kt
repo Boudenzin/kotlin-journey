8c2b8edd5fda1488dbc72c75dc586ba751f6711d
romildo <romildo-rrj@hotmail.com>
2025-06-15 17:31:19 -0300

    feat: Termina a atividade do GridLayout aplicando cores

--- month02-apps/week1/GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.kt
+++ month02-apps/week1/GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -43,7 +45,9 @@ fun GridLayout(modifier: Modifier = Modifier) {
         {
         Row (modifier = Modifier.weight(1f)) {
             Column (modifier = Modifier.weight(1f)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .background(Color(0xFFEADDFF))
+                .padding(16.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
@@ -57,7 +61,9 @@ fun GridLayout(modifier: Modifier = Modifier) {
                 )
             }
             Column (modifier = Modifier.weight(1f)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .background(Color(0xFFD0BCFF))
+                .padding(16.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
@@ -73,7 +79,11 @@ fun GridLayout(modifier: Modifier = Modifier) {
         }
         Row (modifier = Modifier.weight(1f)){
             Column (modifier = Modifier.weight(1f)
-                .fillMaxSize()){
+                .fillMaxSize()
+                .background(Color(0xFFB69DF8))
+                .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
                     text = stringResource(id = R.string.title_3),
                     modifier = Modifier.padding(bottom = 16.dp),
@@ -85,7 +95,11 @@ fun GridLayout(modifier: Modifier = Modifier) {
                 )
             }
             Column (modifier = Modifier.weight(1f)
-                .fillMaxSize()){
+                .fillMaxSize()
+                .background(Color(0xFFF6EDFF))
+                .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally){
                 Text(
                     text = stringResource(id = R.string.title_4),
                     modifier = Modifier.padding(bottom = 16.dp),
