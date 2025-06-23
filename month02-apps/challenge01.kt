8c49d5a7163d5a137a9fbc4724e8d813850c18e8
Romildo Júnior <139826286+Boudenzin@users.noreply.github.com>
2025-06-23 11:00:31 -0300

    feat: Faz mais da metade da solução do desafio 1

--- month02-apps/challenge01.kt
+++ month02-apps/challenge01.kt
@@ -1,36 +1,101 @@
 interface Ligavel {
-    val tipo: String
+    var tipo: String
     
-    open fun ligar()
+    fun ligar()
     
-    open fun desligar()
+    fun desligar()
     
-    open fun status() {
+    fun status() {
         println("Dispositivo do tipo $tipo está em estado desconhecido")
     }
 }
 
-interface Atualizavel
-
-class SmartDevice(name: String, category: String): Ligave {
-    val deviceStatus: String = "offline"
+interface Atualizavel {
     
-    constructor(statusCode: Int): this(name, category) {
+    fun atualizarFirmware() {
+        println("Firmware atualizado com sucesso.")
+    }
+    
+    fun status() {
+        println("Status de atualização desconhecido.")
+    }
+}
+
+open class SmartDevice(name: String, category: String): Ligavel {
+    
+    var name = name
+    
+    override var tipo: String = category
+    
+    override fun ligar() {
+    	deviceStatus = "online"
+    }
+
+    override fun desligar() {
+        deviceStatus = "offline"
+    }
+    
+    var deviceStatus: String = "offline"
+    
+    constructor(name: String, category: String, statusCode: Int): this(name, category) {
         deviceStatus = when(statusCode) {
             0 -> "offline"
             1 -> "online"
-            else -> unknown
+            else -> "unknown"
             
+        }
+    }
+    
+    override fun status() {
+        println("Dispositivo: '$name' ($tipo). Status: $deviceStatus")
+    }
+    
+}
+
+class CentralDeControle() {
+    
+    private val dispositivos = mutableListOf<Ligavel>()
+    
+    fun adicionarDispositivo(d: Ligavel) {
+        println("Dispositivo adicionado")
+        dispositivos.add(d)
+    }
+    fun ligarTodos() {
+        println("Todos os dispositivos foram ligados")
+        for (dispositivo in dispositivos) {
+            dispositivo.ligar()
+        }
+    }
+    fun desligarTodos() {
+        println("Todos os dispositivos foram desligados")
+        for (dispositivo in dispositivos) {
+            dispositivo.desligar()
+        }
+    }
+    fun mostrarStatus() {
+        for (dispositivo in dispositivos) {
+            dispositivo.status()
         }
     }
 }
 
-class SmartTvDevice(name: String, category: String): SmartDevice(name: String, category: String) {
+class SmartTvDevice(name: String, category: String): SmartDevice(name, category), Atualizavel {
     override fun status () {
-        println("A tv está em estado ativo")
+        super<SmartDevice>.status()
+        super<Atualizavel>.status()
+        println("Status completo da Smart TV.")
     }
     fun abrirYoutube() {
         println("Youtube aberto!")
     }
 }
 
+fun main() {
+    val smartTv = SmartTvDevice("TCL Q655", "Android TV")
+    val central = CentralDeControle()
+    
+    central.adicionarDispositivo(smartTv)
+    central.ligarTodos()
+    central.mostrarStatus()
+    central.desligarTodos()
+}
