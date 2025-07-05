a49a4e8393af5d5f5433b5a4ff1d8b038dab8dac
romildo <romildo-rrj@hotmail.com>
2025-07-04 22:18:24 -0300

    feat: Adiciona vetores dos dados

--- month02-apps/week4/DiceRollerApp/app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ month02-apps/week4/DiceRollerApp/app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
 
@@ -29,17 +32,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        content = { innerPadding ->
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {}) {
             Text(
-                text = "Roll the Dice!",
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
+                stringResource(R.string.roll)
             )
         }
-    )
+
+    }
 
 }
 
