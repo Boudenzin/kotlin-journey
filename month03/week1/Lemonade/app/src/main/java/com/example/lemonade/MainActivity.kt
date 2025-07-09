d1fa8a6ec73ba66e306b21a0262b9bbc9072e2dd
romildo.rodrigues <romildo-rrj@hotmail.com>
2025-07-09 12:30:55 -0300

    feat: Adicione a primeira parte do codigo da limonada

--- month03/week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ month03/week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -19,29 +23,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LemonadeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MakeLemonade(modifier: Modifier = Modifier) {
+    val sentenceLemonade = stringResource(R.string.text_phase_1)
+    Column (){
+        Text(
+            text = sentenceLemonade
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadeApp() {
     LemonadeTheme {
-        Greeting("Android")
+        MakeLemonade(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center))
     }
 }
