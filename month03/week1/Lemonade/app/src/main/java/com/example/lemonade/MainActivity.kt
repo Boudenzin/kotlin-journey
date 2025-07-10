47732407b8a968176adc55c24259a1b2dc19f8b8
romildo <romildo-rrj@hotmail.com>
2025-07-10 14:26:00 -0300

    feat: Renomeia as strings e adiciona botão na tela com erro de tamanho

--- month03/week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ month03/week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +35,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MakeLemonade(modifier: Modifier = Modifier) {
-    val sentenceLemonade = stringResource(R.string.text_phase_1)
+
+    var lemonadePhase = 0
+
+    val lemonadeImage = when (lemonadePhase) {
+        0 -> painterResource(id = R.drawable.lemon_tree)
+        1 -> painterResource(id = R.drawable.lemon_squeeze)
+        2 -> painterResource(id = R.drawable.lemon_drink)
+        else -> painterResource(id = R.drawable.lemon_restart)
+    }
+
+    var lemonadeContent = when(lemonadePhase) {
+        0 -> stringResource(id = R.string.lemon_tree_content_description)
+        1 -> stringResource(id = R.string.lemon_squeeze_content_description)
+        2 -> stringResource(id = R.string.lemon_drink_content_description)
+        else -> stringResource(id = R.string.lemon_restart_content_description)
+    }
+
+    var sentenceLemonade = when (lemonadePhase) {
+        0 -> stringResource(id = R.string.lemon_tree)
+        1 -> stringResource(id = R.string.lemon_squeeze)
+        2 -> stringResource(id = R.string.lemon_drink)
+        else -> stringResource(id = R.string.lemon_restart)
+    }
+
     Column (){
+
+        Button(
+            onClick = {lemonadePhase = (lemonadePhase + 1) % 4},
+        ) {
+            Image(
+                painter = lemonadeImage,
+                contentDescription = lemonadeContent,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
+                    .padding(16.dp)
+            )
+        }
+
         Text(
-            text = sentenceLemonade
+            sentenceLemonade,
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+                .padding(16.dp)
 
         )
     }
