16c6021a5df462e9820dc2867c6fe33a0ca6fb41
romildo.rodrigues <romildo-rrj@hotmail.com>
2025-07-11 09:41:58 -0300

    fix: Resolve o problema de centralização do botão

--- month03/week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ month03/week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,48 +50,53 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MakeLemonade(modifier: Modifier = Modifier) {
 
-    var lemonadePhase = 0
+    var lemonadePhase = remember { mutableStateOf(0) }
 
-    val lemonadeImage = when (lemonadePhase) {
+    val lemonadeImage = when (lemonadePhase.value) {
         0 -> painterResource(id = R.drawable.lemon_tree)
         1 -> painterResource(id = R.drawable.lemon_squeeze)
         2 -> painterResource(id = R.drawable.lemon_drink)
         else -> painterResource(id = R.drawable.lemon_restart)
     }
 
-    var lemonadeContent = when(lemonadePhase) {
+    var lemonadeContent = when(lemonadePhase.value) {
         0 -> stringResource(id = R.string.lemon_tree_content_description)
         1 -> stringResource(id = R.string.lemon_squeeze_content_description)
         2 -> stringResource(id = R.string.lemon_drink_content_description)
         else -> stringResource(id = R.string.lemon_restart_content_description)
     }
 
-    var sentenceLemonade = when (lemonadePhase) {
+    var sentenceLemonade = when (lemonadePhase.value) {
         0 -> stringResource(id = R.string.lemon_tree)
         1 -> stringResource(id = R.string.lemon_squeeze)
         2 -> stringResource(id = R.string.lemon_drink)
         else -> stringResource(id = R.string.lemon_restart)
     }
 
-    Column (){
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
 
         Button(
-            onClick = {lemonadePhase = (lemonadePhase + 1) % 4},
+            onClick = {lemonadePhase.value = (lemonadePhase.value + 1) % 4},
+            shape = RoundedCornerShape(48.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(R.color.background_green))
         ) {
             Image(
                 painter = lemonadeImage,
                 contentDescription = lemonadeContent,
                 modifier = Modifier
-                    .fillMaxSize()
                     .wrapContentSize(Alignment.Center)
                     .padding(16.dp)
             )
         }
-
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
             sentenceLemonade,
             modifier = Modifier
-                .fillMaxSize()
                 .wrapContentSize(Alignment.Center)
                 .padding(16.dp)
 
