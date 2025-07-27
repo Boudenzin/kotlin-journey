be99a155edfb640480357193c9a04717c8c8fbf7
romildo <romildo-rrj@hotmail.com>
2025-07-26 22:31:14 -0300

    feat: Inicie o import da raleway family para o projeto

--- month03/week3/GalleryApp/app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ month03/week3/GalleryApp/app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -10,8 +10,24 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.galleryapp.ui.theme.GalleryAppTheme
+
+val ralewayFamily = FontFamily(
+    Font(R.font.raleway_thin, FontWeight.Thin),           // Peso 100
+    Font(R.font.raleway_extralight, FontWeight.ExtraLight), // Peso 200
+    Font(R.font.raleway_light, FontWeight.Light),         // Peso 300
+    Font(R.font.raleway_regular, FontWeight.Normal),      // Peso 400 (Padrão)
+    Font(R.font.raleway_medium, FontWeight.Medium),       // Peso 500
+    Font(R.font.raleway_semibold, FontWeight.SemiBold),   // Peso 600
+    Font(R.font.raleway_bold, FontWeight.Bold),           // Peso 700
+    Font(R.font.raleway_extrabold, FontWeight.ExtraBold), // Peso 800
+    Font(R.font.raleway_heavy, FontWeight.Black)          // Peso 900 ("Heavy" geralmente mapeia para "Black")
+)
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
