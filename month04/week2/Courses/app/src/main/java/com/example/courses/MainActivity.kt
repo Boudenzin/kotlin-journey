2d97a79c99d260f243c8ac6680c6407afeefa979
romildo <romildo-rrj@hotmail.com>
2025-08-21 21:55:21 -0300

    fix: Conserta erro de Topic de Course ficar com uma coluna muito grande

--- month04/week2/Courses/app/src/main/java/com/example/courses/MainActivity.kt
+++ month04/week2/Courses/app/src/main/java/com/example/courses/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.courses
 
 import android.os.Bundle
-import androidx.compose.material3.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,23 +10,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.courses.data.DataSource
@@ -68,18 +70,23 @@ fun CoursesApp() {
 
 @Composable
 fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
-    Card (modifier = Modifier.fillMaxSize()){
+    Card (modifier = Modifier
+        .height(68.dp)
+    ) {
         Row {
             Image(
                 painter = painterResource(id = topic.imageResourceId),
                 contentDescription = null,
+                modifier = Modifier
+                    .size(68.dp),
+                contentScale = ContentScale.Crop
             )
 
             Column (modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)){
 
                 Text(
                     text = LocalContext.current.getString(topic.stringResourceId),
-                    modifier = Modifier.weight(2f).padding(start = 16.dp)
+                    style = MaterialTheme.typography.bodyMedium,
                 )
                 Row (
                     modifier = Modifier.padding(top = 8.dp),
@@ -92,7 +99,8 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
 
                     )
                     Text(
-                        text = topic.numberOfCourses.toString()
+                        text = topic.numberOfCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
                     )
                 }
             }
