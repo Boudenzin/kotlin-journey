84f10100576883bfb18ba8c55bc770b9e3366335
romildo <romildo-rrj@hotmail.com>
2025-08-30 18:35:24 -0300

    feat: Cria o Card com os dados do Super Heroi, falta ainda fazer os dados adicionais

--- month04/week4/SuperHeroes/app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ month04/week4/SuperHeroes/app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -1,4 +1,82 @@
 package com.example.superheroes
 
-class HeroesScreen {
+import android.graphics.Paint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.Hero
+
+@Composable
+fun HeroListItem(hero: Hero, modifier: Modifier = Modifier) {
+    Card (
+        elevation = CardDefaults.cardElevation(2.dp),
+        modifier = modifier
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(72.dp)
+        ){
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall
+                )
+                Text(
+                    text = stringResource(hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box(
+                modifier = Modifier.size(72.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = hero.imageRes),
+                    contentDescription = null,
+                    align = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier.clip(RoundedCornerShape(8.dp))
+                )
+            }
+        }
+    }
+}
+//TODO: FAZER A IMPLEMENTAÇÃO DAS NOVAS DESCRIÇÕES COM UM EXPAND MORE
+@Preview(showBackground = true)
+@Composable
+fun HeroListItemPreview() {
+    // Exemplo de como o Preview ficaria
+    HeroListItem(
+        hero = Hero(
+            nameRes = R.string.hero1,
+            descriptionRes = R.string.description1,
+            imageRes = R.drawable.android_superhero1
+        )
+    )
 }
