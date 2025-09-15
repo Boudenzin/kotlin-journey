497f4964ad01d25eb5921de3ffeb8cf2a887a29d
romildo.rodrigues <romildo-rrj@hotmail.com>
2025-09-15 13:02:39 -0300

    feat: Adiciona dessert clicker ao projeto

---
+++ month05/week1/DessertClicker/basic-android-kotlin-compose-training-dessert-clicker-main/app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
@@ -0,0 +1,10 @@
+package com.example.dessertclicker.data
+
+data class DessertUiState(
+    val currentDessertIndex: Int = 0
+    val dessertsSold: Int = 0,
+    val revenue: Int = 0,
+    val currentDessertPrice: Int = dessertList[currentDessertIndex].price
+
+
+)
