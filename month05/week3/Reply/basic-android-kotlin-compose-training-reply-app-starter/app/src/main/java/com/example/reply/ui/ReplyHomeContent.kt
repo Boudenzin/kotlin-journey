3195794623cb0612a74364e96905293524ec955c
Romildo Júnior <romildo-rrj@hotmail.com>
2025-10-07 11:41:24 -0300

    feat: Crie um layout com conteúdo adaptável do ReplyApp

--- month05/week3/Reply/basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/ui/ReplyHomeContent.kt
+++ month05/week3/Reply/basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/ui/ReplyHomeContent.kt
@@ -120,9 +120,8 @@ fun ReplyListAndDetailContent(
         ReplyDetailsScreen(
             replyUiState = replyUiState,
             modifier = Modifier
-                .padding(top = dimensionResource(R.dimen.email_list_item_vertical_spacing))
                 .weight(1f),
-            onBackPressed = {}
+            onBackPressed = { activity.finish() }
         )
     }
 }
